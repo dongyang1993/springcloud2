8eb6f2c79cce2a713fb84f0cf713ad470d01b719
Henry <dongyang1993@outlook.com>
2021-03-04 11:00:06 +0800

    init

--- cloud-consumer/src/main/java/com/cloud/consumer/controller/ConsumerController.java
+++ cloud-consumer/src/main/java/com/cloud/consumer/controller/ConsumerController.java
@@ -4,6 +4,7 @@ import com.cloud.common.api.Rs;
 import com.cloud.common.api.entity.UserInfo;
 import com.cloud.consumer.feign.ProviderFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/consumer")
+@RequestMapping("/userinfo")
 public class ConsumerController {
     @Autowired
     private ProviderFeignClient providerFeignClient;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping("/listInfo")
     Rs<List<UserInfo>> listInfo(@RequestParam("msg") String msg) {
